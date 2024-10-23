633af284126812a931e1347d73e1213fa4ba1079
Ibrahim <the.ibrahimkhan.mik3301@gmail.com>
2024-10-23 15:29:01 +0530

    Prepared the basic structure of the APIs

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 // APIS
-
+// Signup API - POST to add / insert new user
 app.post("/signup", async (req,res) => {
     // console.log(req.body);
     const userObj = new User(req.body);
@@ -31,9 +31,71 @@ app.post("/signup", async (req,res) => {
     
 })
 
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req,res) => {
+
+    try{
+        const users = await User.find();
+        console.log(users);
+        if(!users)
+        {
+          res.status(404).send("Users not found");
+        }
+        else
+          res.send(users);
+    }
+    catch(err)
+    {
+      res.status(400).send("Something went wrong");
+    }
+})
+
+// User API - GET specific user by email
+app.get("/user", async (req,res) => {
+
+  const userEmail = req.body.emailId;
+  try{
+    const user = await User.findOne({emailId : userEmail});
+    console.log(user);
+
+    if(!user){
+      res.status(404).send("User not found");
+    }
+    else
+      res.send(user);
+  }
+  catch(err){
+    res.status(400).send("Something went wrong");
+  }
+})
+
+// Delete User API - DELETE a specific user
+app.delete("/user", async (req,res) => {
+
+  const userId = req.body.userId;
+  try{
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted sucessfully");
+  }
+  catch(err) {
+    res.status(400).send("Something went wrong");
+  }
+})
+
+// Update User API - PATCH a specific use
+app.patch("/user", async (req,res) => {
+  const userEmail = req.body.emailId;
+  const data = req.body;
+  try{
+    const user = await User.updateOne({emailId : userEmail}, data);
+    res.send("User updated successfully");
+  }catch(err){
+    res.status(400).send("Something went wrong");
+  }
+})
 
 
-
+// Connection to the database
 connectDB()
   .then(() => {
     console.log("Connection to the database established successfully");
